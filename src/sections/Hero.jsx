d9a3aa985eadcfd2db689d7b25017b498fe6d617
Umair-A <umairahmed.ua1999@gmail.com>
2024-01-23 17:28:11 +0500

    classesUpdate

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,15 +12,15 @@ const Hero = () => {
       id='home'
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
       
-      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>Our summer collection</p>
-        <h1 className='mt-10 font-palanquin text-8xl max:sm:text-[72px] max-sm:leading-[82] font-bold'>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
           <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The new arrival</span><br />
-          <span className='text-coral-red inline-block mt-3'> nike </span> shoes
+          <span className='text-coral-red inline-block mt-3'> Nike </span> shoes
         </h1>
-        <p className='text-slate-gray mt-5 mb-14 text-lg sm:max-w-sm'>Discover stylish nike arrivals, comfort for your active life</p>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>Discover stylish nike arrivals, comfort for your active life</p>
         <Button label="Shop now" iconURl={iconURl}/>
-        <div className='flex justify-start items-start flex-wrap w-full gap-16 mt-20'>
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
           {statistics.map((stat) => (
             <div key={stat.label}>
               <p className='text-4xl font-planquin font-bold'>{stat.value}</p>
@@ -35,9 +35,9 @@ const Hero = () => {
           alt='big-shoe'
           width={610}
           height={500}
-          className='object-contain'
+          className='object-contain relative z-10'
         />
-        <div className='flex absolute gap-4 sm:gap-6 -bottom-[10%] sm:left-[10%] max-sm:px-6'>
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
           {shoes.map((shoe) => (
             <div key={shoe}>
               <ShoesCard
